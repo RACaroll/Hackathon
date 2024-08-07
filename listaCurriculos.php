308c09ac06f64df93ceeca8c4247f0e3163dc562
InazumaBoy <claudiofelipesan@gmail.com>
2024-08-07 19:13:59 -0300

    teste

--- listaCurriculos.php
+++ listaCurriculos.php
@@ -9,7 +9,7 @@
 <body>  
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">navbar</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
